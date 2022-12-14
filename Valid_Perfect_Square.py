bad8a24f9d15a02e57e2bea763392e5832a85643
Kanamathareddysravani <98075809+Kanamathareddysravani@users.noreply.github.com>
2022-12-14 15:42:54 +0530

    t=int(input()) for i in range(t):     a=int(input())     flag=0     for j in range(1,a+1):         if a==j*j:             flag=1             break     if (flag==0):         print("False")     else:         print("True")

---
+++ Valid_Perfect_Square.py
@@ -0,0 +1,12 @@
+t=int(input())
+for i in range(t):
+    a=int(input())
+    flag=0
+    for j in range(1,a+1):
+        if a==j*j:
+            flag=1
+            break
+    if (flag==0):
+        print("False")
+    else:
+        print("True")
